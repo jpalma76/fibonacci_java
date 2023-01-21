aa61cda86d624530d0d4078da9923437e382e42f
Juan Palma <juan.palma.alvarez@gmail.com>
2023-01-21 02:28:28 -0300

    se agrega reto fibonacci en python
    Esta es una version del mismo reto pero en python.

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+a=0
+b=1
+c=0
+i=0
+largo_ciclo=int(input('cuantas veces quieres que se repita: '))
+
+while i <= largo_ciclo:
+    if i <= largo_ciclo:
+        print(int(a))
+        i += 1
+        c=a+b
+        a=b
+        b=c
